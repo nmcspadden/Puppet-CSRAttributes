a2626efdf5126bd7e6e4cee32670a9fb1902328c
Nick McSpadden <nmcspadden@gmail.com>
2015-01-16 08:40:01 -0800

    Changed UUID to facter's virtual fact

--- postflight.sh
+++ postflight.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 serial=`system_profiler SPHardwareDataType | awk '/Serial/ {print $4}'`
-uuid=`system_profiler SPHardwareDataType | awk '/UUID/ {print $3}'`
+virtual=`facter virtual'`
 
 sed -e "s/mySerialNumber/$serial/" -i .yaml /etc/puppet/csr_attributes.yaml
-sed -e "s/myUUIDnumber/$uuid/" -i .yaml /etc/puppet/csr_attributes.yaml
+sed -e "s/facter_virtual/$virtual/" -i .yaml /etc/puppet/csr_attributes.yaml
