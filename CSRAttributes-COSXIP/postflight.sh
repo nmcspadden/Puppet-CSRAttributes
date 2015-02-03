7b532a65020ee72f0eac8603879f7757a48abc59
Nick McSpadden <nmcspadden@gmail.com>
2015-02-03 07:50:46 -0800

    Now compatible with OS X install environment

---
+++ CSRAttributes-COSXIP/postflight.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+serial=`system_profiler SPHardwareDataType | awk '/Serial/ {print $4}'`
+virtual="physical"
+
+if [[ `system_profiler SPHardwareDataType | grep VMware` ]]; then
+	virtual="vmware"
+elif`system_profiler SPHardwareDataType | grep VirtualBox` ]]; then
+	virtual="virtualbox"
+elif [[ `system_profiler SPEthernetDataType | grep "/0x1ab8/"` ]]; then
+	virtual="parallels"
+fi
+
+
+sed -e "s/mySerialNumber/$serial/" -i .yaml "$3/etc/puppet/csr_attributes.yaml"
+sed -e "s/facter_virtual/$virtual/" -i .yaml "$3/etc/puppet/csr_attributes.yaml"
--- postflight.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-serial=`system_profiler SPHardwareDataType | awk '/Serial/ {print $4}'`
-virtual=`facter virtual'`
-
-sed -e "s/mySerialNumber/$serial/" -i .yaml /etc/puppet/csr_attributes.yaml
-sed -e "s/facter_virtual/$virtual/" -i .yaml /etc/puppet/csr_attributes.yaml
