de29cb6319cb4158c6acd275fead2ba31d463d75
Nick McSpadden <nmcspadden@gmail.com>
2015-02-03 07:57:48 -0800

    Fixed missing [[ in postflight

--- postflight.sh
+++ postflight.sh
@@ -5,7 +5,7 @@ virtual="physical"
 
 if [[ `system_profiler SPHardwareDataType | grep VMware` ]]; then
 	virtual="vmware"
-elif`system_profiler SPHardwareDataType | grep VirtualBox` ]]; then
+elif [[ `system_profiler SPHardwareDataType | grep VirtualBox` ]]; then
 	virtual="virtualbox"
 elif [[ `system_profiler SPEthernetDataType | grep "/0x1ab8/"` ]]; then
 	virtual="parallels"
